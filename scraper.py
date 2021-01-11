941b1e4397d03c9ae4d5231130f15419a3977961
Jeffe <j.garner200@gmail.com>
2021-01-11 11:17:24 -0500

    scraping of geo coords for news object, if it is on Wikipedia

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from dms2dec.dms_convert import dms2dec
 
 response = requests.get(url="https://en.wikipedia.org/wiki/Main_Page")
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -8,6 +9,16 @@ soup = BeautifulSoup(response.content, 'html.parser')
 news_featured = []
 news_ongoing = []
 news_recent_deaths = []
+
+
+all_news = {
+    "featured": news_featured,
+    "ongoing": news_ongoing,
+    "recent_deaths": news_recent_deaths
+}
+
+# Scraping featured news - Wikipedia main page, the top-right section with the heading "In the news", the dotted ul of recent news
+
 
 wiki_itn_featured = soup.find(id="mp-itn").find_all("li")
 
@@ -22,6 +33,8 @@ for news in wiki_itn_featured:
             {"title": title, "href": "https://en.wikipedia.org" + slug}
         )
 
+# Scraping ongoing news - Wikipedia main page, the top-right section with the heading "In the news", the "Ongoing" section in the footer (immediately below featured news)
+
 wiki_itn_ongoing = soup.find(
     "div",  {"class": "itn-footer"}).find('ul').find_all('a')
 
@@ -31,6 +44,8 @@ for news in wiki_itn_ongoing:
     news_ongoing.append(
         {"title": title, "href": "https://en.wikipedia.org" + slug}
     )
+
+# Scraping ongoing news - Wikipedia main page, the top-right section with the heading "In the news", the "Recent deaths" section in the footer (immediately below ongoing news)
 
 wiki_itn_recent_deaths = soup.find(
     "div",  {"class": "itn-footer"}).find_all('ul')[-1].find_all('a')
@@ -42,11 +57,7 @@ for news in wiki_itn_recent_deaths:
         {"title": title, "href": "https://en.wikipedia.org" + slug}
     )
 
-all_news = {
-    "featured": news_featured,
-    "ongoing": news_ongoing,
-    "recent_deaths": news_recent_deaths
-}
+# For each news object, go to the wikipedia page and scrape the top three paragraphs of main subject content, add it as a value to the news object
 
 for category in all_news.keys():
     for news in all_news[category]:
@@ -58,8 +69,12 @@ for category in all_news.keys():
         topmost_paragraphs_elements = soup.find(
             id="mw-content-text").find_all('p', {'class': ''})[0:3]
 
+        dms_el = soup.find('span', {'class': 'geo-dms'})
+        if dms_el:
+            dms = dms_el.text.split(' ')
+            news['coords'] = {'lat': dms2dec(dms[0]), 'lon': dms2dec(dms[1])}
+            print(news)
+            # dd_str = dms2dec(str(dms.text))
+
         for p_el in topmost_paragraphs_elements:
             news['content'] = p_el.text
-
-
-print(all_news)
